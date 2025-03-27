9cf92e718d603b279cd764f83c4c4832975ec828
BilalAlha <Bilal.alhamami@outlook.com>
2025-03-27 17:45:47 +0100

    Mi primer commit en el proyecto Al Hamami Bilal ED04

---
+++ src/main/java/com/mycompany/hotel/objetos/EmpleadoBAH.java
@@ -0,0 +1,13 @@
+package com.mycompany.hotel.objetos;
+
+public class EmpleadoBAH {
+    public int idEmpleado;
+    public String nombre;
+    public String apellidos;
+    public String DNI;
+    public int numeroTelefono;
+
+    public EmpleadoBAH(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+}
---
+++ src/main/java/com/mycompany/hotel/objetos/HabitacionBAH.java
@@ -0,0 +1,12 @@
+package com.mycompany.hotel.objetos;
+
+public class HabitacionBAH {
+    public int idHabitacion;
+    public String tipo;
+    public int numeroCamas;
+    public double costePorDia;
+
+    public HabitacionBAH(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+}
